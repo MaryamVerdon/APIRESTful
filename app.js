74abc3954618d6fa560821694d8fdc44c86abd8b
Maryam Verdon <maryamverdon@gmail.com>
2022-02-03 22:56:52 +0100

    connexion au Cluster MongoDB - création d'un schéma Thing

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
-app.use(express.json());
 
+mongoose.connect('mongodb+srv://rootmv:toto1@cluster0.orpsb.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,7 +18,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-
+app.use(bodyParser.json());
+  
 app.post('/api/stuff', (req, res, next) => {
     console.log(req.body);
     res.status(201).json({
