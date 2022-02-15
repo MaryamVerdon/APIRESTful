58fd61e0e63f31646b6930366f5e471fc8e63deb
Maryam Verdon <maryamverdon@gmail.com>
2022-02-15 15:51:55 +0100

    modification de la route/controller DELETE : permet de supprimer l'image stocké dans le backend

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/Thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
   const thingObject = JSON.parse(req.body.thing);
@@ -40,33 +41,16 @@ exports.createThing = (req, res, next) => {
   };
   
   exports.deleteThing = (req, res, next) => {
-    Thing.findOne({ _id: req.params.id }).then(
-      (thing) => {
-        if (!thing) {
-          res.status(404).json({
-            error: new Error('No such Thing!')
-          });
-        }
-        if (thing.userId !== req.auth.userId) {
-          res.status(400).json({
-            error: new Error('Unauthorized request!')
-          });
-        }
-        Thing.deleteOne({ _id: req.params.id }).then(
-          () => {
-            res.status(200).json({
-              message: 'Deleted!'
-            });
-          }
-        ).catch(
-          (error) => {
-            res.status(400).json({
-              error: error
-            });
-          }
-        );
-      }
-    )
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => {
+        const filename = thing.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Thing.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+            .catch(error => res.status(400).json({ error }));
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
   };
   
   exports.getAllStuff = (req, res, next) => {
